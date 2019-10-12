c210321dfbe6af51fa2cb2adf63d1ac168d253ed
curist <curist.cyc@gmail.com>
2019-10-12 13:21:03 +0800

    proper setup to play loop

--- src/play.ts
+++ src/play.ts
@@ -1,10 +1,17 @@
 import Tone from 'tone'
 
-export default function play() {
+export default async function play() {
   console.log('play')
+  Tone.Transport.stop()
+
   const synth = new Tone.Synth().toMaster()
 
   //play a middle 'C' for the duration of an 8th note
   synth.triggerAttackRelease("C5", "8n")
-  synth.triggerAttackRelease("C6", "8n")
+
+  const loop = new Tone.Loop(function(time){
+    synth.triggerAttackRelease("C6", "8n", time)
+  }, "4n")
+  loop.start('1m').stop('2m')
+  Tone.Transport.start()
 }
